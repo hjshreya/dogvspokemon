bfcf4adf14908ada32e57ce7db98894e6310a21e
Shreya J <140332122+hjshreya@users.noreply.github.com>
2025-05-15 19:12:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ with open("data/logo.svg", "r") as svg_file:
 # Embed the SVG logo using HTML
 st.markdown(f'<div style="text-align:center">{svg_logo}</div>', unsafe_allow_html=True)
 st.subheader("Find out which Pokémon your dog would fight against") 
+
+# Function to crop and resize the image
+def crop_and_resize(image, size=(255, 255)):
+    # Convert the image to RGBA
+    image = image.convert("RGBA")
+    width, height = image.size
+
+    # Calculate the central crop size
+    new_size = min(width, height)
+    left = (width - new_size) / 2
+    top = (height - new_size) / 2
+    right = (width + new_size) / 2
+    bottom = (height + new_size) / 2
+
+    # Crop the image to the center
+    cropped_image = image.crop((left, top, right, bottom))
+
+    # Resize the cropped image
+    resized_image = cropped_image.resize(size, Image.LANCZOS)
+    return resized_image
