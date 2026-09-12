191dc0dc58d6f5a2872b2fc8d7b1c4090df089bc
Shreya J <140332122+hjshreya@users.noreply.github.com>
2026-09-12 13:50:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ if st.button("Find Pokémon Opponent", disabled=uploaded_image is None):
                 file_name="pokemon_vs_dog_result.png",
                 mime="image/png"
             )
-             social_media_links = []
+            social_media_links = []
 
             social_media_icons = SocialMediaIcons(social_media_links)
             social_media_icons.render()
