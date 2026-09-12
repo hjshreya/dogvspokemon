5298d8755d9424326c0546446d911df23a12fd9a
Shreya J <140332122+hjshreya@users.noreply.github.com>
2026-09-12 13:58:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -151,9 +151,6 @@ if st.button("Find Pokémon Opponent", disabled=uploaded_image is None):
             # Create social media links with st-social-media-links
             social_media_links = [
                 "https://x.com/intent/tweet?text=Check%20out%20the%20result%20of%20the%20battle%20between%20my%20dog%20and%20a%20Pokémon!%20%23DogVsPokemon&url=https://pokemonvsperro.streamlit.app",
-                "https://www.tiktok.com/@izanhacecosas",
-                "https://youtube.com/@quarto.es",
-                "https://www.instagram.com/izanhacecosas/"
             ]
 
             social_media_icons = SocialMediaIcons(social_media_links)
